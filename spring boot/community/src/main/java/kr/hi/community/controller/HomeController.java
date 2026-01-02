efa9fdb2460e8d72812c2cd2aac6f069d0c09877
Kim-yeonhee <hiasis5@naver.com>
2026-01-02 18:21:53 +0900

    1/2 : 스프링부트 4일차 화면구현

--- spring boot/community/src/main/java/kr/hi/community/controller/HomeController.java
+++ spring boot/community/src/main/java/kr/hi/community/controller/HomeController.java
@@ -1,7 +1,12 @@
 package kr.hi.community.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import kr.hi.community.model.dto.MemberDTO;
+import kr.hi.community.service.MemberService;
 
 @Controller
 public class HomeController {
@@ -11,9 +16,41 @@ public class HomeController {
 	 * 
 	 * */
 	
+	//객체를 생성해서 연결. 단, 빈(bean)에 등록된 클래스/인터페이스만
+	//@Bean, @Service, @Repository 등
+	@Autowired
+	MemberService memberService;
+	
 	@GetMapping("/")
 	public String home() {
 		return "index";
 	}
-
-}
+	
+	@GetMapping("/signup")
+	public String signup() {
+		return "signup";
+		
+	}
+	
+	@PostMapping("/signup")
+	//get방식 메서드와 겹치지않게 Post붙여서 처리
+	public String signupPost( /* 화면이 보낸 회원 정보를 받아옴 */ // @리퀘스트파람("id정보")
+		MemberDTO member){
+		
+		boolean result = memberService.signup(member);
+		//회원 가입이 성공하면 메인페이지로
+		if(result) {
+			return "redirect:/";
+		}
+		else {
+			return "redirect:signup";
+		}
+		
+	}
+	@GetMapping("/login")
+	public String login() {
+		return "login";
+	}
+	
+	
+ }
--- spring boot/community/src/main/java/kr/hi/community/dao/MemberDAO.java
+++ spring boot/community/src/main/java/kr/hi/community/dao/MemberDAO.java
@@ -2,8 +2,12 @@ package kr.hi.community.dao;
 
 import org.apache.ibatis.annotations.Param;
 
+import kr.hi.community.model.dto.MemberDTO;
+import kr.hi.community.model.vo.MemberVO;
+
 public interface MemberDAO {
 
-	String selectMember(@Param("id") String id);
+	MemberVO selectMember(@Param("id") String id);
 
+	boolean insertMember(@Param("obj") MemberDTO member); //@Param("매퍼에서사용할이름")
 }
